9b991fa3411b0ab66020c7aff5b2bbbd0a762af9
Christopher Hong <christopherhong2019@gmail.com>
2019-09-04 19:31:30 -0500

    Delelte files manually

--- CS425 Online Automotive Sales/src/query.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-export default async function(text, values) {
-	const { data } = await axios.post('/query', {text: text.trim(), values})
-	return data;
-}
--- CS487 Online Grocery Store/README.md
+++
@@ -1,1 +0,0 @@
-CS487 Spring 2019: Follow a plan-based development process for the online grocery store project.
